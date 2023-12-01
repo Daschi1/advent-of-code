ecb076111e87c6767cf73d93d9524f7b2742c8e7
Daschi <daschi.03@icloud.com>
2023-12-01 16:01:09 +0100

    finished day01 part1

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -4,17 +4,39 @@ import println
 import readInput
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    // part 1
+    fun findFirstNumber(input: String): Int? {
+        for (char in input) {
+            if (char.isDigit()) {
+                return char.digitToInt()
+            }
+        }
+        return null
     }
 
+    fun part1(input: List<String>): Int {
+        var sum = 0;
+        for (s in input) {
+            val first = findFirstNumber(s)
+            val last = findFirstNumber(s.reversed())
+            val concatenated = "$first$last".toInt()
+            sum += concatenated
+        }
+        return sum
+    }
+
+    // part 2
+    val numberRegex = "".toRegex()
+
     fun part2(input: List<String>): Int {
-        return input.size
+return 0
     }
 
     // test if implementation meets criteria from the description:
-    val testInput = readInput("day01/Day01_test")
-    check(part1(testInput) == 142)
+    val testInput1 = readInput("day01/Day01_test1")
+    check(part1(testInput1) == 142)
+    val testInput2 = readInput("day01/Day01_test2")
+    check(part2(testInput2) == 281)
 
     // calculate solution
     val input = readInput("day01/Day01")
