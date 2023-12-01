e04493535d28796301322a2caf9e09d949f79666
Daschi <daschi.03@icloud.com>
2023-12-01 17:17:25 +0100

    solved day01 part2

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -14,26 +14,70 @@ fun main() {
         return null
     }
 
+    fun concatenateFirstAndLastNumber(firstNumberInput: String, lastNumberInput: String): Int? {
+        val first = findFirstNumber(firstNumberInput)
+        val last = findFirstNumber(lastNumberInput.reversed())
+        if (first == null || last == null) return null
+        return "$first$last".toInt()
+    }
+
     fun part1(input: List<String>): Int {
         var sum = 0
         for (s in input) {
-            val first = findFirstNumber(s)
-            val last = findFirstNumber(s.reversed())
-            if (first == null || last == null) continue
-
-            val concatenated = "$first$last".toInt()
+            val concatenated = concatenateFirstAndLastNumber(s, s) ?: continue
             sum += concatenated
         }
         return sum
     }
 
     // part 2
+    fun replaceFirstSpelledNumberWithIntegers(input: String): String {
+        val regex = "one|two|three|four|five|six|seven|eight|nine".toRegex()
+        val matchResult = regex.find(input) ?: return input
 
+        val numericRepresentation = when (matchResult.value) {
+            "one" -> "1"
+            "two" -> "2"
+            "three" -> "3"
+            "four" -> "4"
+            "five" -> "5"
+            "six" -> "6"
+            "seven" -> "7"
+            "eight" -> "8"
+            "nine" -> "9"
+            else -> return input
+        }
+        return input.replaceRange(matchResult.range, numericRepresentation)
+    }
+
+    fun replaceLastSpelledNumberWithIntegers(input: String): String {
+        val inputReversed = input.reversed()
+        val regexReversed = "one|two|three|four|five|six|seven|eight|nine".reversed().toRegex()
+        val matchResult = regexReversed.find(inputReversed) ?: return input
+
+        val numericRepresentation = when (matchResult.value) {
+            "one".reversed() -> "1"
+            "two".reversed() -> "2"
+            "three".reversed() -> "3"
+            "four".reversed() -> "4"
+            "five".reversed() -> "5"
+            "six".reversed() -> "6"
+            "seven".reversed() -> "7"
+            "eight".reversed() -> "8"
+            "nine".reversed() -> "9"
+            else -> return input
+        }
+        val replacedReversedString = inputReversed.replaceRange(matchResult.range, numericRepresentation)
+        return replacedReversedString.reversed()
+    }
 
     fun part2(input: List<String>): Int {
         var sum = 0
         for (s in input) {
-
+            val forwardReplacedString = replaceFirstSpelledNumberWithIntegers(s)
+            val backwardReplacedString = replaceLastSpelledNumberWithIntegers(s)
+            val concatenated = concatenateFirstAndLastNumber(forwardReplacedString, backwardReplacedString) ?: continue
+            sum += concatenated
         }
         return sum
     }
