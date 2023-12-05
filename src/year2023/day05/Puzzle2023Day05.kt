e611ea20cf6c82227adf37d8d3210185d7496ab7
Daschi <daschi.03@icloud.com>
2023-12-05 13:04:33 +0100

    solved year2023 day05 part2

--- src/year2023/day05/Puzzle2023Day05.kt
+++ src/year2023/day05/Puzzle2023Day05.kt
@@ -8,45 +8,11 @@ fun main() {
     puzzle.testAndSolveAndPrint()
 }
 
-class Puzzle2023Day05 : Puzzle<Int, Int>("2023", "05", 35, -1) {
+class Puzzle2023Day05 : Puzzle<Int, Int>("2023", "05", 35, 46) {
     override fun solvePart1(input: List<String>): Int {
         if (input.isEmpty()) return -1
-        "parsing".println()
-        val seeds =
-            input[0].split(":")[1].split(" ").filter { s -> s.all { it.isDigit() } }.mapNotNull { it.toLongOrNull() }
-
-        val converters = mutableListOf<Converter>()
-        var from = ""
-        var to = ""
-        var ranges = mutableListOf<LinkedLongRange>()
-        for (s in input.drop(1)) {
-            if (s.isEmpty()) continue
-
-            if (s.endsWith(":")) {
-                if (ranges.isNotEmpty()) {
-                    val converter = Converter(from, to, ranges)
-                    converters.add(converter)
-                }
-
-                val split = s.split(" ")[0].split("-")
-                from = split[0]
-                to = split[2]
-                ranges = mutableListOf()
-                continue
-            }
-
-            val splitInts = s.split(" ").mapNotNull { it.toLongOrNull() }
-            val linkedLongRange = LinkedLongRange(
-                LongRange(splitInts[1], splitInts[1] + splitInts[2]),
-                LongRange(splitInts[0], splitInts[0] + splitInts[2])
-            )
-            ranges.add(linkedLongRange)
-        }
-        if (ranges.isNotEmpty()) {
-            val converter = Converter(from, to, ranges)
-            converters.add(converter)
-        }
-        "finished parsing".println()
+        val seeds = parseRawSeeds(input[0])
+        val converters = parseConvertersFromInput(input)
 
         var lowestLocation = Long.MAX_VALUE
         for (seed in seeds) {
@@ -60,8 +26,71 @@ class Puzzle2023Day05 : Puzzle<Int, Int>("2023", "05", 35, -1) {
     }
 
     override fun solvePart2(input: List<String>): Int {
-        return input.size
+        if (input.isEmpty()) return -1
+        val rawSeeds = parseRawSeeds(input[0])
+        val seedRanges = mutableListOf<LongRange>()
+        for (i in rawSeeds.indices step 2) {
+            val range = LongRange(rawSeeds[i], rawSeeds[i] + rawSeeds[i + 1] - 1)
+            seedRanges.add(range)
+        }
+        val converters = parseConvertersFromInput(input)
+
+        "converting...".println()
+        var lowestLocation = Long.MAX_VALUE
+        for ((i, seedRange) in seedRanges.withIndex()) {
+            "(${i + 1}/${seedRanges.size}): $seedRange".println()
+            for (seed in seedRange) {
+                var number = seed
+                for (converter in converters) {
+                    number = converter.convertFromTo(number)
+                }
+                if (number < lowestLocation) lowestLocation = number
+            }
+        }
+        "converting finished".println()
+        return lowestLocation.toInt()
     }
+}
+
+private fun parseRawSeeds(input: String): List<Long> {
+    return input.split(":")[1].split(" ").filter { s -> s.all { it.isDigit() } }.mapNotNull { it.toLongOrNull() }
+}
+
+private fun parseConvertersFromInput(input: List<String>): List<Converter> {
+    "parsing...".println()
+    val converters = mutableListOf<Converter>()
+    var from = ""
+    var to = ""
+    var ranges = mutableListOf<LinkedLongRange>()
+    for (s in input.drop(1)) {
+        if (s.isEmpty()) continue
+
+        if (s.endsWith(":")) {
+            if (ranges.isNotEmpty()) {
+                val converter = Converter(from, to, ranges)
+                converters.add(converter)
+            }
+
+            val split = s.split(" ")[0].split("-")
+            from = split[0]
+            to = split[2]
+            ranges = mutableListOf()
+            continue
+        }
+
+        val splitInts = s.split(" ").mapNotNull { it.toLongOrNull() }
+        val linkedLongRange = LinkedLongRange(
+            LongRange(splitInts[1], splitInts[1] + splitInts[2] - 1),
+            LongRange(splitInts[0], splitInts[0] + splitInts[2] - 1)
+        )
+        ranges.add(linkedLongRange)
+    }
+    if (ranges.isNotEmpty()) {
+        val converter = Converter(from, to, ranges)
+        converters.add(converter)
+    }
+    "parsing finished".println()
+    return converters
 }
 
 private data class Converter(val from: String, val to: String, val ranges: List<LinkedLongRange>) {
