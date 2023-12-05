eaf5c5976a08df35a6762840e6c3964e4f7a846f
Daschi <daschi.03@icloud.com>
2023-12-05 12:38:13 +0100

    solved year2023 day05 part1

--- src/year2023/day05/Puzzle2023Day05.kt
+++ src/year2023/day05/Puzzle2023Day05.kt
@@ -1,6 +1,7 @@
 package year2023.day05
 
 import Puzzle
+import println
 
 fun main() {
     val puzzle = Puzzle2023Day05()
@@ -9,8 +10,53 @@ fun main() {
 
 class Puzzle2023Day05 : Puzzle<Int, Int>("2023", "05", 35, -1) {
     override fun solvePart1(input: List<String>): Int {
-        val linkedIntRange = LinkedIntRange(IntRange(5, 10), IntRange(15, 20))
-        return input.size
+        if (input.isEmpty()) return -1
+        "parsing".println()
+        val seeds =
+            input[0].split(":")[1].split(" ").filter { s -> s.all { it.isDigit() } }.mapNotNull { it.toLongOrNull() }
+
+        val converters = mutableListOf<Converter>()
+        var from = ""
+        var to = ""
+        var ranges = mutableListOf<LinkedLongRange>()
+        for (s in input.drop(1)) {
+            if (s.isEmpty()) continue
+
+            if (s.endsWith(":")) {
+                if (ranges.isNotEmpty()) {
+                    val converter = Converter(from, to, ranges)
+                    converters.add(converter)
+                }
+
+                val split = s.split(" ")[0].split("-")
+                from = split[0]
+                to = split[2]
+                ranges = mutableListOf()
+                continue
+            }
+
+            val splitInts = s.split(" ").mapNotNull { it.toLongOrNull() }
+            val linkedLongRange = LinkedLongRange(
+                LongRange(splitInts[1], splitInts[1] + splitInts[2]),
+                LongRange(splitInts[0], splitInts[0] + splitInts[2])
+            )
+            ranges.add(linkedLongRange)
+        }
+        if (ranges.isNotEmpty()) {
+            val converter = Converter(from, to, ranges)
+            converters.add(converter)
+        }
+        "finished parsing".println()
+
+        var lowestLocation = Long.MAX_VALUE
+        for (seed in seeds) {
+            var number = seed
+            for (converter in converters) {
+                number = converter.convertFromTo(number)
+            }
+            if (number < lowestLocation) lowestLocation = number
+        }
+        return lowestLocation.toInt()
     }
 
     override fun solvePart2(input: List<String>): Int {
@@ -18,34 +64,38 @@ class Puzzle2023Day05 : Puzzle<Int, Int>("2023", "05", 35, -1) {
     }
 }
 
-private data class Converter(val from: String, val to: String) {
-    companion object {
+private data class Converter(val from: String, val to: String, val ranges: List<LinkedLongRange>) {
+    fun convertFromTo(input: Long): Long {
+        val linkedIntRange = ranges.find { it.canConvertFromSource(input) }
+        return linkedIntRange?.convertFromSourceToDestination(input) ?: return input
+    }
 
+    fun convertToFrom(input: Long): Long {
+        val linkedIntRange = ranges.find { it.canConvertFromDestination(input) }
+        return linkedIntRange?.convertFromDestinationToSource(input) ?: return input
     }
 }
 
-private data class LinkedIntRange(private val source: IntRange, private val destination: IntRange) {
+private data class LinkedLongRange(private val source: LongRange, private val destination: LongRange) {
     init {
         require(source.count() == destination.count()) { "Source length ${source.count()} and destination length ${destination.count()} must have the same length." }
     }
 
-    fun canConvertFromSource(input: Int): Boolean {
+    fun canConvertFromSource(input: Long): Boolean {
         return source.contains(input)
     }
 
-    fun canConvertFromDestination(input: Int): Boolean {
+    fun canConvertFromDestination(input: Long): Boolean {
         return destination.contains(input)
     }
 
-    fun convertFromSourceToDestination(input: Int): Int {
+    fun convertFromSourceToDestination(input: Long): Long {
         if (!canConvertFromSource(input)) throw IndexOutOfBoundsException("Input $input outside of source range: $source.")
-        val index = source.indexOf(input)
-        return destination.elementAt(index)
+        return destination.first + (input - source.first)
     }
 
-    fun convertFromDestinationToSource(input: Int): Int {
+    fun convertFromDestinationToSource(input: Long): Long {
         if (!canConvertFromDestination(input)) throw IndexOutOfBoundsException("Input $input outside of destination range: $destination.")
-        val index = destination.indexOf(input)
-        return source.elementAt(index)
+        return source.first + (input - destination.first)
     }
 }
