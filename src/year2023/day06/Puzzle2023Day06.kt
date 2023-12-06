c6a6c7a6cc2ac1fb1eb2c14356bcc45580cf23cc
Daschi <daschi.03@icloud.com>
2023-12-06 08:13:17 +0100

    solved year2023 day06 part1

--- src/year2023/day06/Puzzle2023Day06.kt
+++ src/year2023/day06/Puzzle2023Day06.kt
@@ -9,10 +9,37 @@ fun main() {
 
 class Puzzle2023Day06 : Puzzle<Int, Int>("2023", "06", 288, -1) {
     override fun solvePart1(input: List<String>): Int {
-        return input.size
+        if (input.size != 2) return -1
+        val durations = parseInputToIntList(input[0])
+        val records = parseInputToIntList(input[1])
+
+        if (durations.size != records.size) return -1
+        var product = 1
+        for ((i, duration) in durations.withIndex()) {
+            val record = records[i]
+            val race = Race(duration, record)
+            product *= race.calculateAmountOfWaysToWin()
+        }
+        return product
     }
 
     override fun solvePart2(input: List<String>): Int {
         return input.size
+    }
+}
+
+private fun parseInputToIntList(input: String): List<Int> {
+    return input.split(":")[1].split(" ").filter { s -> s.all { it.isDigit() } }.mapNotNull { it.toIntOrNull() }
+}
+
+private data class Race(val duration: Int, val record: Int) {
+    fun calculateAmountOfWaysToWin(): Int {
+        var amountOfWaysToWin = 0
+        for (buttonPressedFor in 0..duration) {
+            val timeToTravel = duration - buttonPressedFor
+            val distance = buttonPressedFor * timeToTravel
+            if (distance > record) amountOfWaysToWin++
+        }
+        return amountOfWaysToWin
     }
 }
