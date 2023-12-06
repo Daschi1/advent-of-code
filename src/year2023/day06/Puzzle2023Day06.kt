d94a783e66152edea8b470b388620bb78f1f8146
Daschi <daschi.03@icloud.com>
2023-12-06 08:21:40 +0100

    solved year2023 day06 part2

--- src/year2023/day06/Puzzle2023Day06.kt
+++ src/year2023/day06/Puzzle2023Day06.kt
@@ -17,14 +17,19 @@ class Puzzle2023Day06 : Puzzle<Int, Int>("2023", "06", 288, 71503) {
         var product = 1
         for ((i, duration) in durations.withIndex()) {
             val record = records[i]
-            val race = Race(duration, record)
+            val race = Race(duration.toLong(), record.toLong())
             product *= race.calculateAmountOfWaysToWin()
         }
         return product
     }
 
     override fun solvePart2(input: List<String>): Int {
-        return input.size
+        if (input.size != 2) return -1
+        val duration = parseInputToLong(input[0])
+        val record = parseInputToLong(input[1])
+
+        val race = Race(duration, record)
+        return race.calculateAmountOfWaysToWin()
     }
 }
 
@@ -32,7 +37,11 @@ private fun parseInputToIntList(input: String): List<Int> {
     return input.split(":")[1].split(" ").filter { s -> s.all { it.isDigit() } }.mapNotNull { it.toIntOrNull() }
 }
 
-private data class Race(val duration: Int, val record: Int) {
+private fun parseInputToLong(input: String): Long {
+    return parseInputToIntList(input).joinToString("").toLong()
+}
+
+private data class Race(val duration: Long, val record: Long) {
     fun calculateAmountOfWaysToWin(): Int {
         var amountOfWaysToWin = 0
         for (buttonPressedFor in 0..duration) {
