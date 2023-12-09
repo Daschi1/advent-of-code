ee3f06846ff991a05a387ea3540709f6497f9132
Daschi <daschi.03@icloud.com>
2023-12-09 10:05:11 +0100

    solved year2023 day09 part1

--- src/year2023/day09/Puzzle2023Day09.kt
+++ src/year2023/day09/Puzzle2023Day09.kt
@@ -9,10 +9,52 @@ fun main() {
 
 class Puzzle2023Day09 : Puzzle<Int, Int>("2023", "09", 114, -1) {
     override fun solvePart1(input: List<String>): Int {
-        return input.size
+        val sequences = input.map { s ->
+            val entries = s.split(" ").map { it.toInt() }.toMutableList()
+            Sequence(entries)
+        }
+        return sequences.sumOf { Sequence.predictNextValueInSequence(it) }
     }
 
     override fun solvePart2(input: List<String>): Int {
         return input.size
+    }
+}
+
+private data class Sequence(private val entries: MutableList<Int>) {
+    companion object {
+        fun predictNextValueInSequence(sequence: Sequence): Int {
+            val sequences = mutableListOf(sequence)
+            while (!sequences.last().isZero()) {
+                sequences.add(sequences.last().createDifferenceSequence())
+            }
+            for (i in (sequences.size - 2) downTo 0) {
+                val difference = sequences[i + 1].getLastEntry()
+                sequences[i].insertNewValue(difference)
+            }
+            return sequences.first().getLastEntry()
+        }
+    }
+
+    fun isZero(): Boolean {
+        return !entries.any { it != 0 }
+    }
+
+    fun getLastEntry(): Int {
+        return entries.last()
+    }
+
+    fun createDifferenceSequence(): Sequence {
+        val differenceEntries = mutableListOf<Int>()
+        for (i in 0..<(entries.size - 1)) {
+            val difference = entries[i + 1] - entries[i]
+            differenceEntries.add(difference)
+        }
+        return Sequence(differenceEntries)
+    }
+
+    fun insertNewValue(difference: Int) {
+        val newValue = entries.last() + difference
+        entries.add(newValue)
     }
 }
