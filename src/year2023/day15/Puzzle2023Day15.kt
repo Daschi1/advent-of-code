6e0ffec6fa096fb0a0a4b03b00e227e8e45b91ae
Daschi <daschi.03@icloud.com>
2023-12-15 08:06:38 +0100

    solved year2023 day15 part1

--- src/year2023/day15/Puzzle2023Day15.kt
+++ src/year2023/day15/Puzzle2023Day15.kt
@@ -9,10 +9,26 @@ fun main() {
 
 class Puzzle2023Day15 : Puzzle<Int, Int>("2023", "15", 1320, -1) {
     override fun solvePart1(input: List<String>): Int {
-        return input.size
+        val steps = input[0].split(",")
+        var sum = 0
+        for (step in steps) {
+            sum += calculateHash(step)
+        }
+        return sum
     }
 
     override fun solvePart2(input: List<String>): Int {
         return input.size
     }
+}
+
+private fun calculateHash(input: String): Int {
+    var currentValue = 0
+    for (c in input.toCharArray()) {
+        val asciValue = c.code
+        currentValue += asciValue
+        currentValue *= 17
+        currentValue %= 256
+    }
+    return currentValue
 }
