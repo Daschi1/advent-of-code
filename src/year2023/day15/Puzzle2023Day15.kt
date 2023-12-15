11feca277cfde1e71a6e650b1eb19d32bed474bd
Daschi <daschi.03@icloud.com>
2023-12-15 09:19:30 +0100

    solved year2023 day15 part2

--- src/year2023/day15/Puzzle2023Day15.kt
+++ src/year2023/day15/Puzzle2023Day15.kt
@@ -9,16 +9,28 @@ fun main() {
 
 class Puzzle2023Day15 : Puzzle<Int, Int>("2023", "15", 1320, 145) {
     override fun solvePart1(input: List<String>): Int {
-        val steps = input[0].split(",")
+        val instructions = input[0].split(",")
         var sum = 0
-        for (step in steps) {
-            sum += calculateHash(step)
+        for (instruction in instructions) {
+            sum += calculateHash(instruction)
         }
         return sum
     }
 
     override fun solvePart2(input: List<String>): Int {
-        return input.size
+        val instructions = input[0].split(",")
+        val boxes = Boxes()
+        for (instruction in instructions) {
+            boxes.processInstruction(instruction)
+        }
+        var sum = 0
+        for (box in boxes.boxes) {
+            for ((i, lens) in box.value.withIndex()) {
+                val focussingPower = (1 + box.key) * (i + 1) * lens.focalLength
+                sum += focussingPower
+            }
+        }
+        return sum
     }
 }
 
@@ -31,4 +43,53 @@ private fun calculateHash(input: String): Int {
         currentValue %= 256
     }
     return currentValue
+}
+
+private class Boxes {
+    val boxes = mutableMapOf<Int, MutableList<Lens>>()
+
+    fun processInstruction(instruction: String) {
+        if (instruction.contains("-")) {
+            val label = instruction.replace("-", "")
+            removeLabel(label)
+        } else if (instruction.contains("=")) {
+            val split = instruction.split("=")
+            val label = split[0]
+            val focalLength = split[1].toInt()
+            addLabel(label, focalLength)
+        }
+    }
+
+    private fun removeLabel(label: String) {
+        val hash = calculateHash(label)
+        if (hash !in boxes) return
+        val lenses = boxes[hash]!!
+        lenses.remove(Lens(label, -1))
+        boxes[hash] = lenses
+    }
+
+    private fun addLabel(label: String, focalLength: Int) {
+        val hash = calculateHash(label)
+        if (hash !in boxes) boxes[hash] = mutableListOf()
+        val lenses = boxes[hash]!!
+        val lens = Lens(label, focalLength)
+        if (lens in lenses) lenses[lenses.indexOf(lens)] = lens
+        else lenses.add(lens)
+        boxes[hash] = lenses
+    }
+}
+
+private data class Lens(val label: String, val focalLength: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (other is Lens) {
+            return other.label == label
+        } else if (other is String) {
+            return other == label
+        }
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return label.hashCode()
+    }
 }
