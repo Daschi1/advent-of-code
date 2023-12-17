801390bb727e1206c0931c54963f272a99d02104
Daschi <daschi.03@icloud.com>
2023-12-17 23:13:27 +0100

    added plain dijkstra year2023 day17 part1

--- src/year2023/day17/Puzzle2023Day17.kt
+++ src/year2023/day17/Puzzle2023Day17.kt
@@ -42,58 +42,101 @@ private fun parseMapFromInput(input: List<String>): Map {
     return Map(height, width, grid)
 }
 
+private data class Node(val y: Int, val x: Int, val distance: Int)
+
+private enum class Direction(val stepY: Int, val stepX: Int) {
+    NORTH(-1, 0),
+    SOUTH(1, 0),
+    WEST(0, -1),
+    EAST(0, 1)
+}
+
 private data class Map(val height: Int, val width: Int, val grid: Array<IntArray>) {
     fun findMostEfficientRouteFromTo(startY: Int, startX: Int, endY: Int, endX: Int): List<Pair<Int, Int>> {
-        val visited = Array(height) { Array(width) { Array(4) { BooleanArray(4) } } }
-        val distance = Array(height) { Array(width) { IntArray(4) { Int.MAX_VALUE } } }
-        val path = Array(height) { Array(width) { Array<Pair<Int, Int>?>(4) { null } } }
-        val directionPath = Array(height) { Array(width) { IntArray(4) { -1 } } }
-        val directions = listOf(Pair(0, 1), Pair(1, 0), Pair(0, -1), Pair(-1, 0))
+        // initial distances all "infinity"
+        val distancesFromStart = Array(height) { IntArray(width) { Int.MAX_VALUE } }
+        val visitedNodes = Array(height) { BooleanArray(width) { false } }
+        // store previousNodes (y, x) of node [y][x] in the path
+        val previousNodes = Array(height) { Array<Pair<Int, Int>?>(width) { null } }
 
-        val queue =
-            PriorityQueue(compareBy<Triple<Pair<Int, Int>, Int, Int>> { distance[it.first.first][it.first.second][it.second] })
-        for (i in 0 until 4) {
-            distance[startY][startX][i] = 0
-            queue.add(Triple(Pair(startY, startX), i, 1))
-        }
+        // define unvisitedNodes to choose lowest distance first
+        val unvisitedNodes = PriorityQueue<Node>(compareBy { it.distance })
+        // setup and add startNode
+        distancesFromStart[startY][startX] = 0
+        val startNode = Node(startY, startX, 0)
+        unvisitedNodes.add(startNode)
 
-        while (queue.isNotEmpty()) {
-            val (current, dir, steps) = queue.poll()
-            val (y, x) = current
-            if (visited[y][x][dir][steps]) continue
-            visited[y][x][dir][steps] = true
+        // while there are nodes to visit
+        while (unvisitedNodes.isNotEmpty()) {
+            val currentNode = unvisitedNodes.poll()
 
-            if (y == endY && x == endX) break
+            // exit if endNode
+            if (currentNode.y == endY && currentNode.x == endX) break
 
-            for ((i, direction) in directions.withIndex()) {
-                val newY = y + direction.first
-                val newX = x + direction.second
-                val newSteps = if (dir == i) steps + 1 else 1
+            // skip currentNode if already been visited, else mark as visited
+            if (visitedNodes[currentNode.y][currentNode.x]) continue
+            else visitedNodes[currentNode.y][currentNode.x] = true
 
-                if (newY in 0 until height && newX in 0 until width && newSteps <= 3 && !visited[newY][newX][i][newSteps]) {
-                    val newDist = distance[y][x][dir] + grid[newY][newX]
-                    if (newDist < distance[newY][newX][i]) {
-                        distance[newY][newX][i] = newDist
-                        path[newY][newX][i] = current
-                        directionPath[newY][newX][i] = dir
-                        queue.add(Triple(Pair(newY, newX), i, newSteps))
-                    }
+            // for each possible next node
+            for (nextDirection in Direction.entries) {
+                val nextY = currentNode.y + nextDirection.stepY
+                val nextX = currentNode.x + nextDirection.stepX
+                // if nextNode is not inside grid bounds or has been visited, skip
+                if (nextY !in 0..<height || nextX !in 0..<width || visitedNodes[nextY][nextX]) continue
+                // calculate nextDistance by using distancesFromStart of currentNode + the cost of travelling to nextNode
+                val nextDistance = distancesFromStart[currentNode.y][currentNode.x] + grid[nextY][nextX]
+                // if nextDistance is better than previous found distance to nextNode
+                if (nextDistance < distancesFromStart[nextY][nextX]) {
+                    // update best distance, set previousNode on best path and add nexNode to unvisitedNodes
+                    distancesFromStart[nextY][nextX] = nextDistance
+                    previousNodes[nextY][nextX] = Pair(currentNode.y, currentNode.x)
+                    val nextNode = Node(nextY, nextX, nextDistance)
+                    unvisitedNodes.add(nextNode)
                 }
             }
         }
+        return reconstructPath(previousNodes, startY, startX, endY, endX)
+    }
 
-        // Reconstruct path
-        val route = mutableListOf<Pair<Int, Int>>()
-        var current: Pair<Int, Int>? = Pair(endY, endX)
-        var currentDir = directions.indices.minByOrNull { distance[endY][endX][it] } ?: return emptyList()
+    private fun reconstructPath(
+        previousNodes: Array<Array<Pair<Int, Int>?>>,
+        startY: Int,
+        startX: Int,
+        endY: Int,
+        endX: Int
+    ): List<Pair<Int, Int>> {
+        val path = mutableListOf<Pair<Int, Int>>()
+        // start with endNode as currentNode
+        var currentY = endY
+        var currentX = endX
 
-        while (current != null && current != Pair(startY, startX)) {
-            route.add(0, current)
-            val prevDir = directionPath[current.first][current.second][currentDir]
-            current = if (prevDir != -1) path[current.first][current.second][prevDir] else null
-            currentDir = prevDir
+        // as long as endNode != startNode, add currentNode to path and look again for currentNode
+        while (currentY != startY || currentX != startX) {
+            path.add(Pair(currentY, currentX))
+            val previous = previousNodes[currentY][currentX] ?: break
+            currentY = previous.first
+            currentX = previous.second
         }
-        route.add(0, Pair(startY, startX)) // Add starting point at beginning
-        return route
+        path.add(Pair(startY, startX))
+
+        return path.reversed()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Map) return false
+
+        if (height != other.height) return false
+        if (width != other.width) return false
+        if (!grid.contentDeepEquals(other.grid)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = height
+        result = 31 * result + width
+        result = 31 * result + grid.contentDeepHashCode()
+        return result
     }
 }
