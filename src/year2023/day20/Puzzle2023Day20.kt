9c53ef0213b8fe25f76f21bb84dface2867b36e6
Daschi <daschi.03@icloud.com>
2023-12-22 22:41:18 +0100

    solved year2023 day20 part2

--- src/year2023/day20/Puzzle2023Day20.kt
+++ src/year2023/day20/Puzzle2023Day20.kt
@@ -7,7 +7,7 @@ fun main() {
     puzzle.testAndSolveAndPrint()
 }
 
-class Puzzle2023Day20 : Puzzle<Int, Int>("2023", "20", 32000000, -1) {
+class Puzzle2023Day20 : Puzzle<Int, Long>("2023", "20", 32000000, -1) {
     override fun solvePart1(input: List<String>): Int {
         val modules = parseModulesFromInput(input)
         val modulesDestinations = parseModulesDestinationsFromInput(input)
@@ -47,10 +47,84 @@ class Puzzle2023Day20 : Puzzle<Int, Int>("2023", "20", 32000000, -1) {
         return amountOfHighPules * amountOfLowPules
     }
 
-    override fun solvePart2(input: List<String>): Int {
+    override fun solvePart2(input: List<String>): Long {
         // no test input / result provided for part2
         if (input.isEmpty()) return -1
-        return input.size
+
+        val modules = parseModulesFromInput(input)
+        val modulesDestinations = parseModulesDestinationsFromInput(input)
+        for ((sourceModule, destinationModules) in modulesDestinations) {
+            for (destinationModule in destinationModules) {
+                val module = modules.getValue(destinationModule)
+                if (module is ConjunctionModule) {
+                    module.mostRecentPulses[sourceModule] = Pulse.LOW
+                }
+            }
+        }
+
+        // Extract the names of all FlipFlop modules.
+        val flipFlopNames = getFlipFlopNames(modules)
+
+        // Calculate results for each binary counter.
+        val binaryCounterResults = modulesDestinations.getValue("broadcaster")
+            .map { name -> calculateBinaryCounterResult(name, modules, modulesDestinations, flipFlopNames) }
+
+        // Combine the results to find the Least Common Multiple (LCM).
+        return binaryCounterResults.reduce(::findLCM)
+    }
+}
+
+private fun findLCM(a: Long, b: Long): Long {
+    val larger = if (a > b) a else b
+    val maxLcm = a * b
+    var lcm = larger
+    while (lcm <= maxLcm) {
+        if (lcm % a == 0L && lcm % b == 0L) {
+            return lcm
+        }
+        lcm += larger
+    }
+    return maxLcm
+}
+
+// Extracts the names of all FlipFlop modules.
+private fun getFlipFlopNames(modules: Map<String, Module>): Set<String> {
+    return modules.values.filterIsInstance<FlipFlopModule>().map { it.name }.toSet()
+}
+
+// Calculates the result for a single binary counter.
+private fun calculateBinaryCounterResult(
+    name: String,
+    modules: Map<String, Module>,
+    modulesDestinations: Map<String, List<String>>,
+    flipFlopNames: Set<String>
+): Long {
+    val startModule = modules.getValue(name)
+    val conjunctionName = findConjunctionForModule(name, modules, modulesDestinations)
+    return generateBinarySequence(startModule, modules, modulesDestinations, flipFlopNames)
+        .map { if (conjunctionName in modulesDestinations.getValue(it.name)) 1 else 0 }
+        .foldIndexed(0L) { index, acc, value -> acc + (value shl index) }
+}
+
+// Finds the conjunction associated with a module.
+private fun findConjunctionForModule(
+    moduleName: String,
+    modules: Map<String, Module>,
+    modulesDestinations: Map<String, List<String>>
+): String {
+    return modulesDestinations.getValue(moduleName)
+        .first { modules.getValue(it) is ConjunctionModule }
+}
+
+// Generates a sequence of modules starting from a given module.
+private fun generateBinarySequence(
+    startModule: Module,
+    modules: Map<String, Module>,
+    modulesDestinations: Map<String, List<String>>,
+    flipFlopNames: Set<String>
+): Sequence<Module> {
+    return generateSequence(startModule) { module ->
+        modules[modulesDestinations.getValue(module.name).firstOrNull { it in flipFlopNames }]
     }
 }
 
