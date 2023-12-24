242060bfdfc0196bd7ea622c17c77ed9fb30f4ab
Daschi <daschi.03@icloud.com>
2023-12-24 11:34:26 +0100

    added boilerplate, input and tests year2023 day24 part1

---
+++ src/year2023/day24/Puzzle2023Day24.kt
@@ -0,0 +1,18 @@
+package year2023.day24
+
+import Puzzle
+
+fun main() {
+    val puzzle = Puzzle2023Day24()
+    puzzle.testAndSolveAndPrint()
+}
+
+class Puzzle2023Day24 : Puzzle<Int, Int>("2023", "24", 2, -1) {
+    override fun solvePart1(input: List<String>): Int {
+        return input.size
+    }
+
+    override fun solvePart2(input: List<String>): Int {
+        return input.size
+    }
+}
