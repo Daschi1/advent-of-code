bcce07d8669fc3d42a62d7d46f519198bb375c0c
Daschi <daschi.03@icloud.com>
2023-12-25 11:50:01 +0100

    solved year2023 day25 part1

--- src/year2023/day25/Puzzle2023Day25.kt
+++ src/year2023/day25/Puzzle2023Day25.kt
@@ -1,6 +1,9 @@
 package year2023.day25
 
 import Puzzle
+import org.jgrapht.alg.StoerWagnerMinimumCut
+import org.jgrapht.graph.DefaultWeightedEdge
+import org.jgrapht.graph.SimpleWeightedGraph
 
 fun main() {
     val puzzle = Puzzle2023Day25()
@@ -8,8 +11,35 @@ fun main() {
 }
 
 class Puzzle2023Day25 : Puzzle<Int, Int>("2023", "25", 54, -1) {
+
+    //** Thanks reddit for suggesting jgrapht */
     override fun solvePart1(input: List<String>): Int {
-        return input.size
+        // Create a simple weighted graph.
+        val graph = SimpleWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge::class.java)
+
+        // Process each line of input to build the graph.
+        input.forEach { line ->
+            // Split the line into vertex name and its connections.
+            val (vertexName, connections) = line.split(": ")
+
+            // Add the vertex to the graph.
+            graph.addVertex(vertexName)
+
+            // Add each connection as a vertex and create an edge between the vertex and its connection.
+            connections.split(" ").forEach { connection ->
+                graph.addVertex(connection)
+                graph.addEdge(vertexName, connection)
+            }
+        }
+
+        // Compute the minimum cut of the graph.
+        val minimumCut = StoerWagnerMinimumCut(graph).minCut()
+        // Calculate the number of vertices in the graph.
+        val totalVertices = graph.vertexSet().size
+        // Calculate the size of the minimum cut.
+        val minCutSize = minimumCut.size
+        // Compute the result based on the graph's vertices and the minimum cut size.
+        return (totalVertices - minCutSize) * minCutSize
     }
 
     override fun solvePart2(input: List<String>): Int {
